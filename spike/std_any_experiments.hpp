70e43f94e197ad27920ffc6570792cc95297f8a5
adewindt <aaron.dewindt@gmail.com>
2020-03-15 03:12:18 +0100

    Added missing files.

---
+++ spike/std_any_experiments.hpp
@@ -0,0 +1,19 @@
+#include <any>
+#include <iostream>
+#include <type_traits>
+#include <utility>
+
+#include "Eigen/Dense"
+
+#include "cw.hpp"
+#include "variables_database.hpp"
+#include "mutable_token.hpp"
+#include "immutable_token.hpp"
+#include "variable.hpp"
+
+
+using namespace cw;
+using namespace cw::sim;
+
+using std::cout;
+using std::endl;
